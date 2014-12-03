41645165bd925f2d794dd04d55cfb922a8fb554e
Pete Corey <petecorey@gmail.com>
2014-12-03 11:24:11 -0800

    Refactoring flex-sections

--- countwith.me.js
+++ countwith.me.js
@@ -1,23 +1,32 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+    // // counter starts at 0
+    // Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
+    // Template.hello.helpers({
+    //     counter: function () {
+    //         return Session.get("counter");
+    //     }
+    // });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
+    // Template.hello.events({
+    //     'click button': function () {
+    //         // increment the counter when button is clicked
+    //         Session.set("counter", Session.get("counter") + 1);
+    //     }
+    // });
+
+    Template.body.helpers({
+        counts: [
+            {number: 123, timestamp: new Date()},
+            {number: 124, timestamp: new Date()},
+            {number: 125, timestamp: new Date()},
+            {number: 126, timestamp: new Date()},
+        ]
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
